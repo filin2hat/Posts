49f378208995a1cda8a6f69dcd7fd608d805570a
Dmitriy Biryulin AN-32 <filin2hat@yandex.ru>
2022-06-28 09:43:42 +0300

    add object WallService.kt

---
+++ src/main/kotlin/WallService.kt
@@ -0,0 +1,25 @@
+object WallService {
+    private var posts = emptyArray<Post>()
+    private var lastID = 0L
+
+
+    fun add(post: Post): Post {
+        posts += post.copy(id = getID())
+        return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        for ((index, thisPost) in posts.withIndex()) {
+            if (thisPost.id == post.id) {
+                posts[index] = post.copy(ownerID = thisPost.ownerID, date = thisPost.date)
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun getID(): Long {
+        lastID += 1
+        return lastID
+    }
+}
