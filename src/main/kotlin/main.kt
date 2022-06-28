643345d2f5de5f1dcfd8c01fa85931a62777deeb
Dmitriy Biryulin AN-32 <filin2hat@yandex.ru>
2022-06-28 10:00:46 +0300

    add main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
-fun main() {
+import java.time.LocalDate
 
+fun main() {
+    val post = WallService.add(
+        Post(
+            1,
+            100,
+            200,
+            777,
+            LocalDate.now(),
+            "First Post",
+            300,
+            99,
+            false, ArrayList(),
+            1,
+            0,
+            10,
+            "post",
+            2
+        )
+    )
+    println(post)
 }
